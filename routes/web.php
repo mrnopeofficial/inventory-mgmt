8bf281810935771a99c313ed6efbf7a64e9f7f45
mrnopeofficial <syahmim4a1@gmail.com>
2022-08-29 14:50:25 +0800

    new welcome page

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use App\Models\Brand;
 use Illuminate\Support\Facades\DB;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
 
 Route::get('/home', function () {
